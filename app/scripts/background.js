99c64d6ca9c1d0574000344df013255ca28cf425
Carlin <email.me@carlinyuen.com>
2016-06-14 01:07:11 -0400

    setting up

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,6 +4,15 @@ chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+// chrome.browserAction.setBadgeText({text: '\'Allo'});
+//
+// console.log('\'Allo \'Allo! Event Page for Browser Action');
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+var URL_WORKSPACE_PAGE = chrome.extension.getURL('workspace.html');
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    return {
+      redirectUrl: URL_WORKSPACE_PAGE + details.url
+    };
+  }, { urls: ["*://docs.google.com/*"] }, ["blocking"]
+);
